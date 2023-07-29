a5eccda23c001e1f7fd5ee39013f261aaeec0b0b
Evelyn Canepa <canepaevelyn@gmail.com>
2023-07-29 14:34:01 -0300

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,15 +3,49 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { IntroComponent } from './intro/intro.component';
+import { AboutUsComponent } from './about-us/about-us.component';
+import { ServicesComponent } from './services/services.component';
+import { WhyUsComponent } from './why-us/why-us.component';
+import { PortfolioComponent } from './portfolio/portfolio.component';
+import { TestimonialsComponent } from './testimonials/testimonials.component';
+import { TeamComponent } from './team/team.component';
+import { ClientsComponent } from './clients/clients.component';
+import { ContactComponent } from './contact/contact.component';
+import { FooterComponent } from './footer/footer.component';
+import { HttpClientModule } from '@angular/common/http';
+import { BoxComponent } from './services/box/box.component';
+import { CommonModule } from '@angular/common';
+import { BlogComponent } from './blog/blog.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { CarouselModule } from 'ngx-owl-carousel-o';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    IntroComponent,
+    AboutUsComponent,
+    ServicesComponent,
+    WhyUsComponent,
+    PortfolioComponent,
+    TestimonialsComponent,
+    TeamComponent,
+    ClientsComponent,
+    ContactComponent,
+    FooterComponent,
+    BoxComponent,
+    BlogComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
-  ],
+    AppRoutingModule,
+    CommonModule,
+    HttpClientModule,
+    BrowserAnimationsModule,
+    CarouselModule,
+      ],
   providers: [],
   bootstrap: [AppComponent]
 })
---
+++ src/app/services/box/box.component.ts
@@ -0,0 +1,20 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-box',
+  templateUrl: './box.component.html',
+  styleUrls: ['./box.component.css']
+})
+export class BoxComponent {
+@Input() delay: String= '';
+@Input() titulo: String = '';
+@Input() texto: String = '';
+@Input() icono: String = '';
+
+
+constructor(){}
+
+ngOnInit(): void{
+  console.log(this.titulo, this.texto)
+}
+}
